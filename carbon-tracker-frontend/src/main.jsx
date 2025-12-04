acb8a0eca221a61214e69588d14070969cb1632f
Tozo10 <sayantanchatterjee00@gmail.com>
2025-12-04 23:02:54 +0530

    Added basic frontend using react and tailwind

--- carbon-tracker-frontend/src/main.jsx
+++ carbon-tracker-frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthForm from './LoginPage.jsx'
+import Dashboard from './dashBoard.jsx'
+import Activity from './activity.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Router>
+      <Routes>
+        {/* Home page → AuthForm */}
+        <Route path="/" element={<AuthForm />} />
+
+        {/* Dashboard after login */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Activity page */}
+        <Route path="/activity" element={<Activity />} />
+      </Routes>
+    </Router>
+
 )
